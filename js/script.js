b1c6b5ac3bd746208bfa8671a5e3ffb5fb5d73f9
tuxubaev17 <tuxubaev17@gmail.com>
2021-07-03 15:00:22 +0500

    Implementation of the script for sending data to

--- js/script.js
+++ js/script.js
@@ -196,7 +196,62 @@ window.addEventListener('DOMContentLoaded', () => {
     new MenuCard(
         'img/tabs/post.jpg', 'post', 'Меню "Постное"', 'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.', 10, '.menu .container'
     ).render();
+    
 
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading : 'Загрузка',
+        success : 'Спасибо. Скоро мы с вами свяжемся',
+        failure : 'Что то пошло не так....'
+    };
+    
+    forms.forEach((e) => {
+        postData(e);
+    });
+
+    function postData(form) {
+
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+
+            request.open('POST', 'server.php');
+            request.getResponseHeader('Content-type', 'application/json; charset=utf8');
+
+            const formData = new FormData(form);
+
+            const object = {};
+            formData.forEach(function(key, value){
+                object[key] = value;
+            });
+
+            const json = JSON.stringify(object);
+            request.send(json);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200){
+                    // 
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000);
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+
+        });
+
+    }
     
     
 });
